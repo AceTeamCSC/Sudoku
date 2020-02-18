7a36f6a471840e1322a42401a4081ddd5eda09d4
sk8asd123 <sk8asd123@gmail.com>
2020-02-17 18:13:50 -0600

    pre-commit works

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -1,4 +1,14 @@
+"""Tests for sudoku.py."""
+
 from sudoku import sudoku
 
+
 def test_foo():
-    assert sudoku.foo('hi') == 'hi'
+    """Pytest tests are callables whose names start with "test".
+
+    Args:
+        None.
+
+    """
+    # More on pytest: https://github.com/pluralsight/intro-to-pytest  #noqa
+    assert sudoku.foo("hi") == "hi"
